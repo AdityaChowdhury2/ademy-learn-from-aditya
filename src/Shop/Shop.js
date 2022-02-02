f02457dfbf7d7f978ebcc84b169df8d99ef46b94
Aditya Chowdhury <adityac5102@gmail.com>
2022-02-02 18:33:23 +0600

    responsive design done

--- src/Shop/Shop.js
+++ src/Shop/Shop.js
@@ -5,8 +5,8 @@ const Shop = (props) => {
     // console.log();
     const { name, description, image, price } = props.course
     return (
-        <Col xs={4} className='mb-4'>
-            <Card style={{ width: '25rem', height: '30rem' }}>
+        <Col md={4} sm={6} className='mb-4' >
+            <Card >
                 <Card.Img variant="top" src={image} />
                 <Card.Body>
                     <Card.Title>{name} <Badge bg="secondary">New</Badge></Card.Title>
@@ -17,7 +17,7 @@ const Shop = (props) => {
                     <Button variant="success" onClick={() => props.handleAddCourse(props.course)}>Add Course</Button>
                 </Card.Body>
             </Card>
-        </Col>
+        </Col >
     );
 };
 
